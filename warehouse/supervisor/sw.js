6e6969cb742e879f0400cc50c78094eda39e80f1
papamohammed77-glitch <papamohammed77@gmail.com>
2026-06-17 15:56:41 +0300

    انشاء تطبيق مشرف مخازن

---
+++ warehouse/supervisor/sw.js
@@ -0,0 +1,24 @@
+var CACHE_NAME = 'warehouse-supervisor-v1';
+var urlsToCache = ['/supervisor.html'];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) return response;
+      return fetch(event.request).then(function(response) {
+        if (!response || response.status !== 200 || response.type !== 'basic') return response;
+        var responseToCache = response.clone();
+        caches.open(CACHE_NAME).then(function(cache) { cache.put(event.request, responseToCache); });
+        return response;
+      });
+    })
+  );
+});
